66f4a7aec32841deb30545afbc2f88b6358e312d
Victoria Pershick <victoriapershick@OH-VPershick2.local>
2018-07-20 17:51:27 -0400

    Initial commit

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+# Development Notes
+
+Node Version:  10.6.0
+NPM Version:  6.1.0
---
+++ src/app/app.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AppService {
+  constructor(private _http: Http) { }
+
+  getTasks() {
+    return this._http.get('assets/api/tasks.json')
+      .map((response: Response) => response.json())
+      .do(data => console.log('All; ' + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+
+//   putNewTask(data) {
+//       return this._http.post('assets/api/tasks.json', data)
+//         .do(data => console.log('All; ' + JSON.stringify(data)))
+//         .catch(this.handleError);
+//   }
+
+  private handleError(error: Response) {
+    console.log(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+}
