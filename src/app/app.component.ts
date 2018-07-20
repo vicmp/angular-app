2e36810946b24b0036d18e1325367578ecd0b20f
Victoria Pershick <victoriapershick@OH-VPershick2.local>
2018-07-20 17:51:27 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Task } from './task.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title = 'My Angular 2 implementation';
+  tasks = [];
+
+  constructor(private _appService: AppService, private _fb: FormBuilder){}
+  
+  public myForm: FormGroup;
+
+  ngOnInit() {
+    this._appService.getTasks()
+      .subscribe(resTasksData => this.tasks = resTasksData);
+    
+    this.myForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(1)]],
+      date: ['', [Validators.required, Validators.minLength(1)]],
+      assigned: ['', [Validators.required, Validators.minLength(1)]]
+    });
+  }
+
+  save(model: Task) {
+    this.tasks.unshift(model);
+    
+    //cannot save new array to json without needing a web server (or simulate w json-server)
+    // this._appService.putNewTask(this.tasks)
+    //   .subscribe(resTasksData => this.tasks = resTasksData);
+  }
+
+  remove(dataIndex) {
+    this.tasks.splice(dataIndex, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpModule, ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
